8b6d773945975e9caba60b517b8deb9e94d36db8
SHIVAM KUMAR GUPTA <120335161+shivamgt821@users.noreply.github.com>
2023-05-11 20:20:06 +0530

    Create Audi.java

---
+++ Audi.java
@@ -0,0 +1,30 @@
+public class Audi extends Car{
+
+	@Override
+	public void start() {
+		
+		System.out.println("Audi car button start");
+	}
+	@Override
+	public void accelerate() {
+		
+		System.out.println("Audi car half padel accelerate");
+	}
+	@Override
+	public void stop() {
+		
+		System.out.println("Audi car button stop");
+	}
+	public void speaker() {
+		
+		System.out.println("Audi car speaker");
+	}
+	public void bluetooth() {
+		
+		System.out.println("Audi car bluetooth is connected");
+	}
+	public void airbag() {
+		
+		System.out.println("Audi car airbag");
+	}
+}
