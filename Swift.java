019c03f4b84042bd2fd124b9c85f9e089476ef0f
SHIVAM KUMAR GUPTA <120335161+shivamgt821@users.noreply.github.com>
2023-05-11 20:21:10 +0530

    Create Swift.java

---
+++ Swift.java
@@ -0,0 +1,31 @@
+public class Swift extends Car {
+	
+	@Override
+	public void start() {
+		
+		System.out.println("Swift car key start");
+	}
+	@Override
+	public void accelerate() {
+		
+		System.out.println("Swift car Half clutch  drive");
+	}
+	@Override
+	public void stop() {
+		
+		System.out.println("Swift car key stop");
+	}
+	public void speaker() {
+		
+		System.out.println("Swift car speaker");
+	}
+	public void bluetooth() {
+		
+		System.out.println("Swift car bluetooth is connected");
+	}
+	public void airbag() {
+		
+		System.out.println("Swift car airbag");
+	}
+
+}
